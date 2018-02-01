efa2afb14a39a0d134413cafd2a9c4e413945055
Kon Yee <konyee95@gmail.com>
2018-02-01 13:05:34 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Import a lirary to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create a component
+const App = () => (
+    <Header />
+);
+
+//Render it to the device
+AppRegistry.registerComponent('Test',() => App);
